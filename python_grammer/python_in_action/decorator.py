beb1cc77fdcd4a1cde7ea1512a4b4bda848bb344
owari-taro <taro.biwajima@gmail.com>
2020-02-20 16:04:36 +0900

    freeze class with decorate

--- python_grammer/python_in_action/decorator.py
+++ python_grammer/python_in_action/decorator.py
@@ -1,5 +1,7 @@
 from functools import lru_cache
 from time import sleep
+from dataclasses import dataclass
+
 
 @lru_cache(maxsize=32)
 def heavy_function(n):
@@ -7,7 +9,16 @@ def heavy_function(n):
     return n+1
 
 
-if __name__=="__main__":
+@dataclass(frozen=True)
+class Fruit:
+    name: str
+    price: int = 0
+
+
+if __name__ == "__main__":
     print(heavy_function(10))
-    #second time ,it respond much faster thanks to cache
+    # second time ,it respond much faster thanks to cache
     print(heavy_function(10))
+    fruit = Fruit("aaa", 123)
+    print(fruit.name)
+    fruit.name = "apple"
