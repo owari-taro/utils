1474a89482bcf583d3bb471dac0fe985e7127e15
owari-taro <taro.biwajima@gmail.com>
2020-02-21 11:28:02 +0900

    donwload function

--- python_grammer/python_in_action/concurrent_practice.py
+++ python_grammer/python_in_action/concurrent_practice.py
@@ -2,9 +2,11 @@ from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import Future
 from random import uniform
 from hashlib import md5
-from pathib import Path
+from pathlib import Path
 from urllib import request
 import time
+from create_decorator import elapsed_time
+from typing import List, Any
 
 
 def test_function():
@@ -13,18 +15,24 @@ def test_function():
 
 def download(url):
     req = request.Request(url)
-    name = md5(url.encode("utf-8"))
+    name = md5(url.encode("utf-8")).hexdigest()
     file_path = f"./{name}"
     with request.urlopen(req)as res:
         Path(file_path).write_bytes(res.read())
-        return url, file_pa
+        return url, file_path
 
+
+@elapsed_time
+def sequential(urls: List):
+    for url in urls:
+        try:
+            download(url)
+        except Exception as err:
+            print(url)
+            print(err)
 
 if __name__ == "__main__":
-
-    future = ThreadPoolExecutor().submit(test_function)
-    print(isinstance(future, Future))
-    print(future.result())
-    print(future.done())
-    ########################################
-
+    download("https://www.google.com")
+    urls = ["https://www.google.com", "https://www.amazon.co.jp/", "http://gmail.com/",
+            "https://toyota.jp/index.html", "https://twitter.com/junsaito0529"]
+    sequential(urls)
