42a301f146322af98a1ce10d9d8e41d92e1e7a51
owari-taro <taro.biwajima@gmail.com>
2020-02-21 12:21:15 +0900

    mutithread

--- python_grammer/python_in_action/concurrent_practice.py
+++ python_grammer/python_in_action/concurrent_practice.py
@@ -7,6 +7,7 @@ from urllib import request
 import time
 from create_decorator import elapsed_time
 from typing import List, Any
+from concurrent.futures import as_completed
 
 
 def test_function():
@@ -14,25 +15,35 @@ def test_function():
 
 
 def download(url):
-    req = request.Request(url)
-    name = md5(url.encode("utf-8")).hexdigest()
-    file_path = f"./{name}"
-    with request.urlopen(req)as res:
-        Path(file_path).write_bytes(res.read())
-        return url, file_path
+    try:
+        req = request.Request(url)
+        name = md5(url.encode("utf-8")).hexdigest()
+        file_path = f"./{name}"
+        with request.urlopen(req)as res:
+            Path(file_path).write_bytes(res.read())
+            return url, file_path
+    except Exception as err:
+        print(url)
+        print(err)
 
 
 @elapsed_time
 def sequential(urls: List):
     for url in urls:
-        try:
-            download(url)
-        except Exception as err:
-            print(url)
-            print(err)
+        download(url)
+
+@elapsed_time
+def get_multi_thread(urls):
+    with ThreadPoolExecutor(max_workers=3)as executor:
+        futures = [executor.submit(download, url) for url in urls]
+        for futures in as_completed(futures):
+            print(futures.result)
+
 
 if __name__ == "__main__":
     download("https://www.google.com")
     urls = ["https://www.google.com", "https://www.amazon.co.jp/", "http://gmail.com/",
-            "https://toyota.jp/index.html", "https://twitter.com/junsaito0529"]
+            "https://toyota.jp/index.html", "https://twitter.com/junsaito0529","https://www.nissan.co.jp/"
+            ,"https://www.honda.com/"]
     sequential(urls)
+    get_multi_thread(urls)
